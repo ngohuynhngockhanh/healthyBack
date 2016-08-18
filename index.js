8654250d5f6dba50735dc7ce6f557bb48c48164c
root <ngohuynhngockhanh@gmail.com>
2016-08-18 09:45:40 +0000

    fix lib

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ var board = new five.Board({
 var bmp180_lib = require("./lib/bmp180");
 
 const SELECT_PIN = ['GP28', 'GP111', 'GP109'];
-const BMP180_COUNT = 4;
+const BMP180_COUNT = 5;
 
 var bmp180 = new bmp180_lib({
 	select_pins: SELECT_PIN,
 	sensor_count: BMP180_COUNT,
-	debug: true,
-	freq: 2000,
-	board: board
+	//debug: true,
+	//freq: 2000,
+	board: board,
+	default_pressure: [45734, 90039, 86889, 91889, 97966 ]
 });
 
 
@@ -26,5 +27,7 @@ board.on("ready", function() {
 	
 	bmp180.start();
 
-	
+	setInterval(function() {
+		console.log(bmp180.getResult());
+	}.bind(this), 1000);
 });
