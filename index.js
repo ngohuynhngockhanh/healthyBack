3bd8881d69602940018b3ea295ade87f61d98a00
root <ngohuynhngockhanh@gmail.com>
2016-08-18 08:34:37 +0000

    test 8 bmp180 - done

--- index.js
+++ index.js
@@ -4,24 +4,41 @@ var board = new five.Board({
   io: new Edison()
 });
 
+const SELECT_PIN = ['GP28', 'GP111', 'GP109'];
+const BMP180_COUNT = 4;
+
+
 board.on("ready", function() {
+	//pinMode bmp180 select
+	for (var i = 0; i < SELECT_PIN.length; i++)
+		this.pinMode(SELECT_PIN[i], five.Pin.OUTPUT);
+	
+	
+	//define function
+	var id = 0;
+	var toogleBMP = function() {
+		console.log("Select BMP " + id);
+		for (var i = 0; i < SELECT_PIN.length; i++)
+			if ((id >> i) & 1)
+				this.digitalWrite(SELECT_PIN[i], 1);
+			else 
+				this.digitalWrite(SELECT_PIN[i], 0);
+		
+		
+		if (++id >= BMP180_COUNT)
+			id = 0;
+	}.bind(this);
 
-  // Plug the BMP180 Barometer module
-  // into an I2C jack
-  var options = {
-    controller: "BMP180",
-	freq: 1000
-  }
-  var barometer = five.IMU.Drivers.get(this, options.controller, options);
+	toogleBMP();
+	var barometer = five.IMU.Drivers.get(this, "BMP180", {});
 
-  barometer.emit("start");
-  barometer.on("data", function(data) {
-	  this.emit("pause");
-    console.log("barometer");
-    console.log("  pressure     : ", data.pressure);
-    console.log("--------------------------------------");
-	 setTimeout(function() {
-		 this.emit("resume");
-	 }.bind(this), 1000);
-  });
+	barometer.emit("start");
+	barometer.on("data", function(data) {
+	    this.emit("pause");
+		console.log(data.pressure);
+		toogleBMP();
+		setTimeout(function() {
+			this.emit("resume");
+		}.bind(this), 20);
+	});
 });
