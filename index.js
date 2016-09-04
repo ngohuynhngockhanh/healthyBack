8e8e73eefc3d12d7e9666ce74cd4826d00b1642a
root <ngohuynhngockhanh@gmail.com>
2016-09-04 03:42:00 +0000

    migrate controller

--- index.js
+++ index.js
@@ -5,32 +5,27 @@ var board = new five.Board({
 });
 
 //lib
-var bmp180_lib = require("./lib/bmp180");
+
 var controller_lib = require("./lib/Controller.js");
 
 const SELECT_PIN = ['GP28', 'GP111', 'GP109'];
 const BMP180_COUNT = 1;
 
-var bmp180 = new bmp180_lib({
-	select_pins: SELECT_PIN,
-	sensor_count: BMP180_COUNT,
-	//debug: true,
-	//freq: 2000, 
+var controller = new controller_lib({
 	board: board,
-	default_pressure: [45734+54536+500, 90069, 86889, 91889-480, 97966 ]
+	bmp_select_pin: ['GP28', 'GP111', 'GP109'],
+	bumps: [{
+		vibrate: ['GP80', 'GP81'],
+		bump: 'GP182',
+		lock: 'GP115',
+		pressureDelta: 100770
+	}]
 });
 
 
 
 
 
-
 board.on("ready", function() {
-	/*this.pinMode('GP115', five.Pin.OUTPUT);
-	this.digitalWrite('GP115', 1);*/
-	bmp180.start();
-
-	setInterval(function() {
-		console.log(bmp180.getResult());
-	}.bind(this), 1000);
+	
 });
