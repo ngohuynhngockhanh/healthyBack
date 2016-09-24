f91fcb89a890dc9b0043f2e2fbaeefbbf09974f3
root <ngohuynhngockhanh@gmail.com>
2016-09-24 05:33:22 +0000

    hihia

--- index.js
+++ index.js
@@ -14,14 +14,6 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-	console.log("socket connected!")
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
-		console.log(data);
-	});
-});
-
 //lib
 
 var controller_lib = require("./lib/Controller.js");
@@ -35,31 +27,64 @@ var controller = new controller_lib({
 		vibrate: ['GP81', 'GP80'],
 		bump: 'GP15',
 		lock: 'GP83',
-		pressureDelta: 100740 - 210,
-		maxPressure: 8100
+		pressureDelta: 100740 - 210
 	},{
 		vibrate: ['GP79', 'GP78'],
 		bump: 'GP49',
 		lock: 'GP82',
-		pressureDelta: 108770 + 500 - 14634 - 220,
-		maxPressure: 8100
+		pressureDelta: 108770 + 500 - 14634 - 220
 	},{
 		vibrate: ['GP41', 'GP42'],
 		bump: 'GP47',
 		lock: 'GP43',
-		pressureDelta: 100770 - 8248,
-		maxPressure: 8100
+		pressureDelta: 100770 - 8248
 	},{
 		vibrate: ['GP84', 'GP128'],
 		bump: 'GP45',
 		lock: 'GP40',
-		pressureDelta: 108770 + 500 -10936,
-		maxPressure: 8100
+		pressureDelta: 108770 + 500 -10936
 	}]
 });
 
 
 
 board.on("ready", function() {
+	
+});
+
+
+
+io.on('connection', function (socket) {
+	console.log("socket connected!")
+	socket.emit('news', { hello: 'world' })
+	socket.emit('current profile', controller.getCurrentProfile())
+	socket.emit('machine status', controller.isRunning, controller.isPause)
+	
+
+	
+	//events
+	socket.on("update profile", function(profile) {
+		controller.updateProfile(profile)
+	})
+	
+	socket.on('machine status', function(isRunning, isPause) {
+		var key = false
+		if (controller.isResume(isRunning, isPause)) {
+			key = true
+			controller.emit('resume')
+		}
+		controller.updateMachineStatus(isRunning, isPause)
+		if (!key)
+			if (controller.isStart())
+				controller.emit('start')
+			else if (controller.isStop())
+				controller.emit('stop')
+			else if (controller.isMachinePause())
+				controller.emit('pause')
+		io.sockets.emit('machine status', controller.isRunning, controller.isPause)
+	});
+	
+	
+	
 	
 });
