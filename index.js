c260421596471d4f6a9176eeeb88f2c5598d7eb9
root <ngohuynhngockhanh@gmail.com>
2016-12-05 11:10:32 +0000

    fix 595

--- index.js
+++ index.js
@@ -20,43 +20,28 @@ app.get('/', function (req, res) {
 
 var controller_lib = require("./lib/Controller.js");
 
-const SELECT_PIN = ['GP115', 'GP109', 'GP111'];
+const SELECT_PINS = ['GP182', 'GP13', 'GP165'];// all are 1v8
+const SHIFTOUT_PINS = {
+	data: 'GP44',
+	clock: 'GP46',
+	latch: 'GP48',
+};
+const MPU6050_ENABLE_PIN =['GP27'] ;
 
 var controller = new controller_lib({
 	board: board,
-	bmp_select_pin: SELECT_PIN,
-	bumps: [{
-		vibrate: ['GP81', 'GP80'],
-		bump: 'GP15',
-		lock: 'GP83',
-		pressureDelta: 100740 - 210
-	},{
-		vibrate: ['GP79', 'GP78'],
-		bump: 'GP49',
-		lock: 'GP82',
-		pressureDelta: 108770 + 500 - 14634 - 220 + 2000
-	},{
-		vibrate: ['GP41', 'GP42'],
-		bump: 'GP47',
-		lock: 'GP43',
-		pressureDelta: 100770 - 8248 + 1000
-	},{
-		vibrate: ['GP84', 'GP128'],
-		bump: 'GP45',
-		lock: 'GP40',
-		pressureDelta: 108770 + 500 -10936 + 4190 +2200
-	},{
-		vibrate: ['GP114', 'GP110'],
-		bump: 'GP130',
-		lock: 'GP129',
-		pressureDelta: 108770 + 500 -10936 + 4190 -12160 + 3333
-	}]
+	bmp_select_pin: SELECT_PINS,
+	shiftoutPins: SHIFTOUT_PINS,
+	MPU6050_enablePin: MPU6050_ENABLE_PIN,
+	bumps_count: 5,
+	vibrate_count: 2,
+	pressureDelta: [101010, 101010, 101010, 101010, 101010]
 });
 
 
 
 board.on("ready", function() {
-	this.pinMode('GP46', five.Pin.OUTPUT);
+	/*this.pinMode('GP46', five.Pin.OUTPUT);
 	this.digitalWrite('GP46', 1);
 	var imu = new five.IMU({
 		controller: "MPU6050",
@@ -66,7 +51,7 @@ board.on("ready", function() {
 		//console.log(this.gyro.pitch); 
 		console.log(this.gyro.pitch);
 		io.sockets.emit("pitch", this.gyro.pitch);
-	}); 
+	});*/ 
 });
 
 
